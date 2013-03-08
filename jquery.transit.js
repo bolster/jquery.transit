d7614b9aea0b6ab37efc71dc8ef18f4c1afbf5cc
Adam Miskiewicz <adam.skevy@mac.com>
2013-03-08 08:58:31 -0500

    updating json and making bower

--- jquery.transit.js
+++ jquery.transit.js
@@ -9,7 +9,7 @@
 
 (function($) {
   $.transit = {
-    version: "0.9.9",
+    version: "0.9.9.1",
 
     // Map of $.css() keys to values for 'transitionProperty'.
     // See https://developer.mozilla.org/en/CSS/CSS_transitions#Properties_that_can_be_animated
