6193210db142cc651c0cc8d52e665b6be82698b4
Adam Miskiewicz <adam.skevy@mac.com>
2013-03-08 09:14:21 -0500

    updating version

--- jquery.transit.js
+++ jquery.transit.js
@@ -9,7 +9,7 @@
 
 (function($) {
   $.transit = {
-    version: "0.9.9.1",
+    version: "0.9.91",
 
     // Map of $.css() keys to values for 'transitionProperty'.
     // See https://developer.mozilla.org/en/CSS/CSS_transitions#Properties_that_can_be_animated
